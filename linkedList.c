27db84c1527b4a31daa79f24f51338137871ea3c
arjun <arjun259194@gmail.com>
2022-03-26 15:55:55 +0530

    created 2 nodes and added a traverse function
    added a traverse function to print the list in console and added 2 nodes to check the function.

--- linkedList.c
+++ linkedList.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
-struct node{
+#include <malloc.h>
+struct node
+{
   int data;
-  struct node* next;
+  struct node *next;
 };
-int main(){
-  
- return 0; 
+
+void listTraverse(struct node *head)
+{
+  struct node *ptr = head;
+  while (ptr != NULL)
+  {
+    printf("%d ", ptr->data);
+    ptr = ptr->next;
+  }
+  printf("EOL\n");
+}
+
+int main()
+{
+  // creating and initializing nodes
+  struct node *head = (struct node *)malloc(sizeof(struct node));
+  head->data = 5;
+  head->next = NULL;
+  struct node *second = (struct node *)malloc(sizeof(struct node));
+  second->data = 6;
+  second->next = NULL;
+
+  // connecting nodes
+  head->next = second;
+
+  listTraverse(head);
+  return 0;
 }
